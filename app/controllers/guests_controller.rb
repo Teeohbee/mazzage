77848fa1d18565b729651382f946199116a46175
Toby Clarke <teeohbee@gmail.com>
2019-01-05 15:44:41 +0000

    Generate guests controller

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,19 @@
+class GuestsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
