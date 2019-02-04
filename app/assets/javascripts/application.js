05d2e145a2edb3df479675194e0ea40d4c229e8a
Toby Clarke <teeohbee@gmail.com>
2019-02-04 10:22:30 +0000

    Set up scaffold for menu items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+  })
+})
