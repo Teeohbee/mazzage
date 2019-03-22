f620f047d5291f0c8d588c148da7a1ea769539a9
Toby Clarke <teeohbee@simplybusiness.co.uk>
2019-03-22 10:17:02 +0000

    Update game data to include background image and only load on the gam page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,68 +1,75 @@
 //= require ./phaser.min.js
+$(document).on('turbolinks:load', function() {
+  const urlPath = $(location).attr("pathname")
 
-const gameState = {
-  musicPlaying: 'off'
-}
+  const gameState = {
+    musicPlaying: 'off'
+  }
 
-function preload() {
-  this.load.image('codey', '/assets/moley.png');
-  this.load.audio('theme', 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Tchaikovsky_-_Dance_of_the_Sugar_Plum_Fairy_-_The_Nutcracker.ogg');
-}
+  function preload() {
+    this.load.image('moley', '/assets/moley.png');
+    this.load.image('moley_bg', '/assets/mole_burrow.jpg');
+    this.load.audio('theme', 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Tchaikovsky_-_Dance_of_the_Sugar_Plum_Fairy_-_The_Nutcracker.ogg');
+  }
 
-function create() {
-  gameState.codey = this.add.sprite(150, 200, 'codey')
-  gameState.musicText = this.add.text(340, 470, 'Music')
-  gameState.cursors = this.input.keyboard.createCursorKeys();
-  gameState.musicText.setInteractive()
-  gameState.music = this.sound.add('theme')
+  function create() {
+    this.add.image(364, 256, 'moley_bg');
+    gameState.moley = this.add.sprite(100, 300, 'moley')
+    gameState.musicText = this.add.text(650, 475, 'Music')
+    gameState.cursors = this.input.keyboard.createCursorKeys();
+    gameState.musicText.setInteractive()
+    gameState.music = this.sound.add('theme')
 
-  gameState.musicText.on('pointerup', function () {
-    if (gameState.musicPlaying == 'off') {
-      gameState.music.play()
-      gameState.musicPlaying = 'on'
-    } else {
-      gameState.music.stop()
-      gameState.musicPlaying = 'off'
-    }
-  })
-}
+    gameState.musicText.on('pointerup', function () {
+      if (gameState.musicPlaying == 'off') {
+        gameState.music.play()
+        gameState.musicPlaying = 'on'
+      } else {
+        gameState.music.stop()
+        gameState.musicPlaying = 'off'
+      }
+    })
+  }
 
-function update() {
-  // Update based on keypress here!
- if (gameState.cursors.right.isDown) {
-   if(gameState.codey.flipX == true) {
-     gameState.codey.x += 5;
-   } else {
-     gameState.codey.toggleFlipX();
-     gameState.codey.x += 5;
+  function update() {
+    // Update based on keypress here!
+   if (gameState.cursors.right.isDown) {
+     if(gameState.moley.flipX == true) {
+       gameState.moley.x += 5;
+     } else {
+       gameState.moley.toggleFlipX();
+       gameState.moley.x += 5;
+     }
    }
- }
- if (gameState.cursors.left.isDown) {
-   if(gameState.codey.flipX == false) {
-     gameState.codey.x -= 5;
-   } else {
-     gameState.codey.toggleFlipX();
-     gameState.codey.x -= 5;
-   } }
- if (gameState.cursors.up.isDown) {
-   gameState.codey.y -= 5;
- }
- if (gameState.cursors.down.isDown) {
-   gameState.codey.y += 5;
- }
-}
+   if (gameState.cursors.left.isDown) {
+     if(gameState.moley.flipX == false) {
+       gameState.moley.x -= 5;
+     } else {
+       gameState.moley.toggleFlipX();
+       gameState.moley.x -= 5;
+     } }
+   if (gameState.cursors.up.isDown) {
+     gameState.moley.y -= 5;
+   }
+   if (gameState.cursors.down.isDown) {
+     gameState.moley.y += 5;
+   }
+  }
 
-const config = {
-  parent: 'idofcontainer',
-	type: Phaser.AUTO,
-	width: 400,
-	height: 500,
-	backgroundColor: "#5f2a55",
-	scene: {
-    preload,
-    create,
-    update
-	}
-}
+  const config = {
+    parent: 'idofcontainer',
+  	type: Phaser.AUTO,
+  	width: 728,
+  	height: 512,
+  	backgroundColor: "#5f2a55",
+  	scene: {
+      preload,
+      create,
+      update
+  	}
+  }
 
-const game = new Phaser.Game(config)
+  if(urlPath == "/pages/gam") {
+    const game = new Phaser.Game(config)
+  }
+})
