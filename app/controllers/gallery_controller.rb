c989ec17f292f441d0c3b5c87d27cb93bcc61e78
Toby Clarke <teeohbee@gmail.com>
2019-08-17 11:42:43 +0100

    Redirect /gallery to our google photos album

---
+++ app/controllers/gallery_controller.rb
@@ -0,0 +1,6 @@
+class GalleryController < ApplicationController
+
+  def index
+    redirect_to "https://photos.app.goo.gl/pLBteJrCQpNWCiNR7"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :guests, except: [:show, :index]
+
   get 'guests/backoffice'
   get 'pages/welcome'
   get 'pages/accommodation'
   get 'pages/faq'
   get 'pages/gam'
+  get 'gallery' => 'gallery#index'
 
   root 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
